4815540c10054ed88c92c9b2f140d22253e779df
Darío Medina <dariomedina10@gmail.com>
2018-10-04 15:14:43 -0400

    details tipo_empleado

--- Control_asistencia_mvc/Controllers/TipoEmpleadoController.cs
+++ Control_asistencia_mvc/Controllers/TipoEmpleadoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,9 +18,20 @@ namespace Control_asistencia_mvc.Controllers
         }
 
         // GET: TipoEmpleado/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var tipo_empleado = db.tipo_empleado.Find(id);
+            if (tipo_empleado == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(tipo_empleado);
         }
 
         // GET: TipoEmpleado/Create
